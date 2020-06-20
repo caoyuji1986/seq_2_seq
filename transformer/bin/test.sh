308dae581384693c03d58a8f1c65eb450e66677d
yuji.cyj <1007171627@qq.com>
2020-06-20 23:00:51 +0800

    增加infer 脚本

---
+++ transformer/bin/test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=$PYTHONPATH:src/
+
+cd ../
+nohup python3.6 src/infer.py \
+  --model_dir=./out/iwslt2016 \
+  --data_dir=dat/iwslt2016/de_en/ \
+  --bpe_model_file=dat/iwslt2016/de_en/model \
+  --model_type=transformer \
+  --model_config=./cfg/transformer.json > log.txt 2>&1 &
+cd -
+
